f899de885c96aba6d3155b9037cc90e896f1ce58
Steven Buss <steven.buss@gmail.com>
2014-06-01 16:53:51 -0700

    Exclude NA

--- line_filters.py
+++ line_filters.py
@@ -1,3 +1,4 @@
 exclude_wrong = lambda line: line['accuracy'] == '0'
-exclude_data = lambda line: (int(line['response_time']) < 200 or
+exclude_data = lambda line: (line['response_time'] == 'NA' or
+                             int(line['response_time']) < 200 or
                              int(line['response_time']) > 2000)
