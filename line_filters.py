fdb3212c12274962373d341bffd9dfa37fd83262
Steven Buss <steven.buss@gmail.com>
2014-06-01 17:25:37 -0700

    Exclude blank response_time

--- line_filters.py
+++ line_filters.py
@@ -1,5 +1,6 @@
 exclude_wrong = lambda line: line['accuracy'] == '0'
 exclude_response_time_out_of_range = lambda line: (
     line['response_time'] == 'NA' or
+    not line['response_time'] or
     int(line['response_time']) < 200 or
     int(line['response_time']) > 2000)
