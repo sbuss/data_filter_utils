de758e49549a09b250d5611c9b8a7a99389cf5fe
Steven Buss <steven.buss@gmail.com>
2014-06-01 20:01:11 -0700

    Move trt stuff to a new module

--- reader.py
+++ reader.py
@@ -1,13 +1,8 @@
 """Give me a directory and I'll process all csvs in it."""
-import argparse
 import csv
 from os import walk
 from os import path
-from os import makedirs
 import re
-
-import line_filters
-from stats import meanstdv
 
 
 patterns = {
@@ -22,7 +17,7 @@ def files_in_dir(dirname, pattern):
                 yield path.join(dirpath, filename)
 
 
-def filter(infile_name, filters, outfile_name):
+def filter_file(infile_name, filters, outfile_name):
     reader = csv.DictReader(open(infile_name, 'rU'))
     writer = csv.DictWriter(open(outfile_name, 'w'), reader.fieldnames)
     writer.writeheader()
@@ -38,60 +33,17 @@ def filter(infile_name, filters, outfile_name):
 
 
 def get_float_values(infile_name, field_name):
-    values = []
     infile = csv.DictReader(open(infile_name, 'rU'))
-    for line in infile:
+    return to_float(line[field_name] for line in infile)
+
+
+def to_float(iterable):
+    values = []
+    for elem in iterable:
         try:
-            value = float(line[field_name])
+            value = float(elem)
         except:
             pass
         else:
             values.append(value)
     return values
-
-
-def filter_all_trt(dirname):
-    """Filter all TRT csv files in `dirname`."""
-    outdir = 'data/trt'
-    try:
-        makedirs(outdir)
-    except:
-        pass
-    for infile_name in files_in_dir(dirname, patterns['trt']):
-        print("Filtering %s" % infile_name)
-        path, name = infile_name.rsplit("/", 1)
-        trt_name = name.rsplit(".", 1)[0]
-        outfile_name = "data/trt/%s-filtered.csv" % trt_name
-        try:
-            # To do the std-dev filtering we have to do a first pass over the
-            # data first.
-            response_times = get_float_values(infile_name, 'response_time')
-            mean, stddev = meanstdv(response_times)
-            std_dev_filter = line_filters.exclude_std_dev(
-                mean, stddev, max_sigma=2.5)
-            filter(infile_name,
-                   [line_filters.exclude_wrong,
-                    line_filters.exclude_response_time_out_of_range,
-                    std_dev_filter],
-                   outfile_name)
-        except Exception as e:
-            print("Couldn't parse %s correctly." % infile_name)
-            print(e)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Parse data files")
-    parser.add_argument(
-        "data_dir",
-        help="The directory which contains all of your data files.")
-    parser.add_argument(
-        "-f", "--filetype",
-        choices=['trt'],
-        help="The filetype.")
-    args = parser.parse_args()
-    if not args.filetype:
-        raise ValueError("You must specify a file type.")
-    if args.filetype.lower() == 'trt':
-        filter_all_trt(args.data_dir)
-    else:
-        raise ValueError("Invalid filetype.")
---
+++ trt.py
@@ -0,0 +1,52 @@
+import argparse
+import csv
+from itertools import groupby
+from os import makedirs
+
+import line_filters
+from reader import filter_file
+from reader import files_in_dir
+from reader import get_float_values
+from reader import to_float
+from stats import meanstdv
+
+
+trt_filename_pattern = r'trt.*\.csv'
+
+
+def filter_all_trt(dirname):
+    """Filter all TRT csv files in `dirname`."""
+    outdir = 'data/trt'
+    try:
+        makedirs(outdir)
+    except:
+        pass
+    for infile_name in files_in_dir(dirname, trt_filename_pattern):
+        print("Filtering %s" % infile_name)
+        path, name = infile_name.rsplit("/", 1)
+        trt_name = name.rsplit(".", 1)[0]
+        outfile_name = "data/trt/%s-filtered.csv" % trt_name
+        try:
+            # To do the std-dev filtering we have to do a first pass over the
+            # data first.
+            response_times = get_float_values(infile_name, 'response_time')
+            mean, stddev = meanstdv(response_times)
+            std_dev_filter = line_filters.exclude_std_dev(
+                mean, stddev, max_sigma=2.5)
+            filter_file(infile_name,
+                        [line_filters.exclude_wrong,
+                         line_filters.exclude_response_time_out_of_range,
+                         std_dev_filter],
+                        outfile_name)
+        except Exception as e:
+            print("Couldn't parse %s correctly." % infile_name)
+            print(e)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Parse data files")
+    parser.add_argument(
+        "data_dir",
+        help="The directory which contains all of your data files.")
+    args = parser.parse_args()
+    filter_all_trt(args.data_dir)
