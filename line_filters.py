dda56331ce8ae50db041ddfeef301766f03a155e
Steven Buss <steven.buss@gmail.com>
2014-06-01 17:24:19 -0700

    More verbose filter name

--- line_filters.py
+++ line_filters.py
@@ -1,4 +1,5 @@
 exclude_wrong = lambda line: line['accuracy'] == '0'
-exclude_data = lambda line: (line['response_time'] == 'NA' or
-                             int(line['response_time']) < 200 or
-                             int(line['response_time']) > 2000)
+exclude_response_time_out_of_range = lambda line: (
+    line['response_time'] == 'NA' or
+    int(line['response_time']) < 200 or
+    int(line['response_time']) > 2000)
