f01fadf2d36320682ee18c5ae79587f7a93ab118
Steven Buss <steven.buss@gmail.com>
2014-06-11 23:49:26 -0700

    Don't crash on divide-by-zero

--- stats.py
+++ stats.py
@@ -13,10 +13,16 @@ def meanstdv(x):
     """
     from math import sqrt
     n, mean, std = len(x), 0, 0
+    if n == 0:
+        return AvgAndStd('null', 'null')
+
     for a in x:
         mean = mean + a
     mean = mean / float(n)
     for a in x:
         std = std + (a - mean)**2
+    if n == 1:
+        return AvgAndStd(mean, 'null')
+
     std = sqrt(std / float(n-1))
     return AvgAndStd(mean, std)
