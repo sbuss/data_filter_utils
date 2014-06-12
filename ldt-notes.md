b4ea246f5c1086035630b58d8ab30171c0d002b1
Steven Buss <steven.buss@gmail.com>
2014-06-12 13:11:29 -0700

    Add ldt script

---
+++ ldt-notes.md
@@ -0,0 +1,13 @@
+# Filtering
+
+Exclude practice (first XXX)
+Just correct response filtering
+
+# Summarizing
+
+Accuracy & RT of nonwords & words & overall
+
+Nonword if:
+    image.startswith('j')
+word if:
+    image.startswith('word')
---
+++ ldt.py
@@ -0,0 +1,60 @@
+import argparse
+import csv
+from copy import deepcopy
+
+from reader import filtered_reader
+from summary import summarize_all
+from summary import summarize_reader
+
+ldt_filename_pattern = r'ldt.*\.csv'
+
+
+def word_nonword_reader(reader):
+    """Annotate each line in a reader with congruent/incongruent"""
+    for line in reader:
+        word_nonword_line = deepcopy(line)
+        word_nonword_line['word_or_nonword'] = ''
+        if line['image'].startswith('j'):
+            word_nonword_line['word_or_nonword'] = 'nonword'
+        elif line['image'].startswith('word'):
+            word_nonword_line['word_or_nonword'] = 'word'
+        yield word_nonword_line
+
+
+def summarize_ldt(filename):
+    """Filter and summarize ldt data.
+
+    Exclude the first XXX lines of practice
+    Do normal correct response filtering
+    Group data on congruent and incongruent
+    """
+    path, name = filename.rsplit("/", 1)
+    participant_name = name.rsplit(".", 1)[0]
+
+    # ldt files are all weird with two heading rows...
+    reader = open(filename, 'rU')
+    reader.readline()
+    reader.readline()
+    reader = filtered_reader(
+        csv.DictReader(reader),
+        filters=[],
+        exclude_lines=5)
+    data = summarize_reader(
+        participant_name, word_nonword_reader(reader), 'word_or_nonword')
+    data['participant'] = participant_name
+    return [data]
+
+
+def summarize_all_ldt(dirname):
+    outfile_name = 'ldt-summary.csv'
+    summarize_all(
+        dirname, ldt_filename_pattern, outfile_name, summarize_ldt)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Parse data files")
+    parser.add_argument(
+        "data_dir",
+        help="The directory which contains all of your data files.")
+    args = parser.parse_args()
+    summarize_all_ldt(args.data_dir)
