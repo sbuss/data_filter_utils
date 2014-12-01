c2c5314c0cd982da07073e8d8a21662806170470
Steven Buss <steven.buss@gmail.com>
2014-12-01 00:07:06 -0800

    Exclude simon data <200 > 1500

--- simon.py
+++ simon.py
@@ -11,6 +11,8 @@ from summary import summarize_reader
 simon_filename_pattern = r'.*((?i)SIMON).*\.csv'
 
 center_filter = lambda line: line['alignment'] == 'center'
+range_filter = lambda line: (
+    int(line['response_time']) < 200 or int(line['response_time']) > 1500)
 
 
 def cog_incog_reader(reader):
@@ -38,7 +40,7 @@ def summarize_simon(filename, std_dev_filter=None):
     path, name = filename.rsplit("/", 1)
     simon_name = name.split("_", 1)[0]
 
-    filters = [center_filter]
+    filters = [center_filter, range_filter]
     if std_dev_filter:
         filters.append(std_dev_filter)
     reader = filtered_reader(
@@ -55,7 +57,7 @@ def summarize_all_simon(dirname):
     outfile_name = 'simon-summary.csv'
     std_dev_filter = get_dir_mean_std_filter(
         dirname, simon_filename_pattern, min_sigma=2.5, max_sigma=2.5,
-        exclude_lines=24, filters=[center_filter])
+        exclude_lines=24, filters=[center_filter, range_filter])
     summarize_fn = partial(summarize_simon, std_dev_filter=std_dev_filter)
     summarize_all(
         dirname, simon_filename_pattern, outfile_name, summarize_fn)
